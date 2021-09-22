46c2045e10bfc15649188d3ade58520ed69fe387
artreysan <al2173038770@azc.uam.mx>
2021-09-22 14:13:15 -0500

    Update

---
+++ programas_Java/primerPrograma/Prueba.java
@@ -0,0 +1,13 @@
+public class Prueba { 
+
+    public static void main(String args[]) { 
+    
+        int valor1 = 10; 
+        int valor2 = 7; 
+        int valor3 = 20; 
+        
+        System.out.println(valor1+valor2); 
+        System.out.println(valor2-valor3); 
+        System.out.println(valor1*valor3); 
+    }   
+} 
